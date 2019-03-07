7255f885925e7ccfd2242da80306deb26e0c25bf
CV Chiranthan <42867826+chiranthancv95@users.noreply.github.com>
2019-03-07 17:07:08 +0530

    Add files via upload

---
+++ Get Alpha/GetAlpha.py
@@ -0,0 +1,17 @@
+
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+from PIL import Image
+
+image=Image.open('outImgonly.png')
+image = image.convert('RGBA')	
+# # First create the image with alpha channel
+# rgba = cv2.cvtColor(rgb_data, cv2.COLOR_RGB2RGBA)
+
+# # Then assign the mask to the last channel of the image
+# rgba[:, :, 3] = alpha_data
+
+data = np.array(image)
+im2 = Image.fromarray(data)
+im2.save("outImgonly(rgba).png")
